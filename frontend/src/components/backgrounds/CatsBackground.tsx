d66bf0ada161280794d9e8d5272cf91580f5e73b
Yuri Mauricio <sangeandyasha04@gmail.com>
2026-07-15 01:15:06 +0800

    added more cats

--- frontend/src/components/backgrounds/CatsBackground.tsx
+++ frontend/src/components/backgrounds/CatsBackground.tsx
@@ -1,4 +1,5 @@
 import React, { useMemo } from 'react';
+import { motion } from 'framer-motion';
 
 const CatsBackground: React.FC = () => {
   const catStickers = useMemo(() => [
@@ -34,8 +35,8 @@ const CatsBackground: React.FC = () => {
     {
       id: 'cat5',
       image: '/cat5.svg',
-      left: 88,
-      top: 8,
+      left: 47.5,
+      top: 7.8,
       size: 90,
       rotation: -4,
       opacity: 0.85,
@@ -44,31 +45,135 @@ const CatsBackground: React.FC = () => {
     {
       id: 'cat9',
       image: '/cat9.gif',
-      left: 5,
-      top: 45,
-      size: 150,
+      left: 18,
+      top: 32.4,
+      size: 200,
       rotation: -8,
       opacity: 0.9,
       flip: false,
     },
+    {
+      id: 'cat11',
+      image: '/cat11.gif',
+      left: 26.6,
+      top: 25,
+      size: 85,
+      rotation: 3,
+      opacity: 0.85,
+      flip: false,
+    },
+    {
+      id: 'cat12',
+      image: '/cat12.png',
+      left: 80,
+      top: 24.6,
+      size: 100,
+      rotation: 0,
+      opacity: 0.8,
+      flip: true,
+    },
+    {
+      id: 'cat13',
+      image: '/cat13.png',
+      left: 39,
+      top: 58.3,
+      size: 100,
+      rotation: 4,
+      opacity: 0.85,
+      flip: false,
+    },
+    {
+      id: 'cat14',
+      image: '/cat14.png',
+      left: 55,
+      top: 25,
+      size: 75,
+      opacity: 0.8,
+      flip: true,
+    },
+    {
+      id: 'cat15',
+      image: '/cat15.png',
+      left: 30.7,
+      top: 40,
+      size: 250,
+      rotation: 2,
+      opacity: 0.85,
+      flip: false,
+    },
+    {
+      id: 'cat16',
+      image: '/cat16.png',
+      left: 72,
+      top: 24.4,
+      size: 100,
+      rotation: 0,
+      opacity: 0.85,
+      flip: false,
+    },
+    {
+      id: 'cat17',
+      image: '/cat17.png',
+      left: 70,
+      top: 47.7,
+      size: 150,
+      rotation: 0,
+      opacity: 0.8,
+      flip: true,
+    },
+    {
+      id: 'cat18',
+      image: '/cat18.png',
+      left: 45,
+      top: 87.9,
+      size: 150,
+      rotation: 2,
+      opacity: 0.85,
+      flip: false,
+    },
+    {
+      id: 'cat19',
+      image: '/cat19.png',
+      left: 2,
+      top: 40,
+      size: 200,
+      rotation: 3,
+      opacity: 0.8,
+      flip: true,
+    },
   ], []);
 
+  // Walking cats with motion animation
   const walkingCats = useMemo(() => [
     {
       id: 'cat7',
       image: '/cat7.gif',
-      left: 10,
-      top: 85,
+      direction: 'left-to-right' as const,
+      yPercent: 97.4,
+      speed: 25,
       size: 90,
       flip: true,
+      delay: 0,
     },
     {
       id: 'cat8',
       image: '/cat8.gif',
-      left: 80,
-      top: 82,
+      direction: 'right-to-left' as const,
+      yPercent: 96.8,
+      speed: 22,
       size: 95,
+      flip: true,
+      delay: 3,
+    },
+    {
+      id: 'cat10',
+      image: '/cat10.gif',
+      direction: 'right-to-left' as const,
+      yPercent: 97,
+      speed: 28,
+      size: 130,
       flip: false,
+      delay: 1.5,
     },
   ], []);
 
@@ -85,7 +190,7 @@ const CatsBackground: React.FC = () => {
         zIndex: 10,
       }}
     >
-      {/* Cat stickers */}
+      {/* Cat stickers (static) */}
       {catStickers.map((cat) => (
         <div
           key={cat.id}
@@ -109,29 +214,44 @@ const CatsBackground: React.FC = () => {
         </div>
       ))}
 
-      {/* Walking cats */}
-      {walkingCats.map((cat) => (
-        <div
-          key={cat.id}
-          style={{
-            position: 'absolute',
-            left: `${cat.left}%`,
-            top: `${cat.top}%`,
-            width: cat.size,
-            height: cat.size,
-            transform: cat.flip ? 'scaleX(-1)' : 'scaleX(1)',
-          }}
-        >
-          <img
-            src={cat.image}
-            alt={`walking cat ${cat.id}`}
-            style={{ width: '100%', height: '100%', objectFit: 'contain', userSelect: 'none' }}
-            draggable={false}
-          />
-        </div>
-      ))}
+      {/* Walking cats - crossing paths with animation */}
+      {walkingCats.map((cat) => {
+        const isRightToLeft = cat.direction === 'right-to-left';
+        const startX = isRightToLeft ? 110 : -10;
+        const endX = isRightToLeft ? -10 : 110;
+        
+        return (
+          <motion.div
+            key={cat.id}
+            style={{
+              position: 'absolute',
+              top: `${cat.yPercent}%`,
+              width: cat.size,
+              height: cat.size,
+              transform: cat.flip ? 'scaleX(-1)' : 'scaleX(1)',
+            }}
+            initial={{ left: `${startX}%` }}
+            animate={{ left: `${endX}%` }}
+            transition={{
+              left: {
+                duration: cat.speed,
+                repeat: Infinity,
+                delay: cat.delay,
+                ease: 'linear',
+              },
+            }}
+          >
+            <img
+              src={cat.image}
+              alt={`walking cat ${cat.id}`}
+              style={{ width: '100%', height: '100%', objectFit: 'contain', userSelect: 'none' }}
+              draggable={false}
+            />
+          </motion.div>
+        );
+      })}
 
-      {/* Paw prints */}
+      {/* Paw prints (static) */}
       {Array.from({ length: 8 }, (_, i) => (
         <div
           key={`paw-${i}`}
